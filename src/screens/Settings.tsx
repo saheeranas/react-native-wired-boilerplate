a3c348228b450348dbde563dd845e6be5d8cf204
Saheer Anas <saheer09anas@gmail.com>
2022-06-07 13:28:58 +0530

    1. Update readme image 2. Add profile info to Settings screen

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {useTheme, Button} from 'react-native-paper';
+import {
+  useTheme,
+  Button,
+  Surface,
+  Avatar,
+  Title,
+  Text,
+  Divider,
+} from 'react-native-paper';
 import {useAuth} from '../hooks/AuthProvider';
 
 import FocusAwareStatusBar from '../components/FocusAwareStatusBar';
@@ -16,11 +24,17 @@ const Settings = () => {
         barStyle="light-content"
         backgroundColor={colors.accent}
       />
-      <View>
-        <Button mode="contained" onPress={signout}>
-          Signout
-        </Button>
-      </View>
+      <Surface style={{backgroundColor: colors.surface}}>
+        <View style={styles.menuWrp}>
+          <Avatar.Text size={64} label="XD" />
+          <Title>Saheer A</Title>
+          <Text>demo@demo.com</Text>
+          <Button mode="text" onPress={signout}>
+            Signout
+          </Button>
+          <Divider />
+        </View>
+      </Surface>
     </SafeAreaView>
   );
 };
@@ -30,7 +44,12 @@ export default Settings;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  menuWrp: {
     justifyContent: 'center',
     alignItems: 'center',
+    paddingTop: 50,
+    paddingBottom: 10,
+    paddingHorizontal: 16,
   },
 });
