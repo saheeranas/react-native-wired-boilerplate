c1d9d213ac5dcfc2546199d028a2460a50e2b950
Saheer Anas <saheer09anas@gmail.com>
2023-02-07 17:52:01 +0530

    Add type to RoundedButton component

--- src/components/RoundedButton.tsx
+++ src/components/RoundedButton.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {Button} from 'react-native-paper';
+import {Button, ButtonProps} from 'react-native-paper';
 
-export default function RoundedButton(props) {
+export default function RoundedButton(props: ButtonProps) {
   return <Button theme={{roundness: 20}} {...props} />;
 }
